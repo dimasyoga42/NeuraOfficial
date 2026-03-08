88eb862659a43c042abdfeb06f41653b7ea93346
dimasyoga42 <dimasyoga9090@gmail.com>
2026-03-08 21:25:35 +0700

    `feat: add CORS support to the Express application`

--- main.js
+++ main.js
@@ -3,10 +3,18 @@ import dotenv from "dotenv";
 import { struckRes } from "./src/utility/struck.js";
 import main from "./src/route/main.js";
 import toram from "./src/route/toram.js";
+import cors from "cors";
 dotenv.config();
 
 const Port = process.env.PORT || 3000;
 const app = express();
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use("/api/etc", main);
 app.use("/api/toram", toram);
 app.listen(Port, () => {
